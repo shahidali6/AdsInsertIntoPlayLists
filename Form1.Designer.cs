727db4122d8eae2e0d68ce6df590e3a051aa64dd
Shahid Saddique <shahidali6@gmail.com>
2021-01-13 00:15:29 +0500

    Initial draft added.

---
+++ Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace AdsIntoPlayList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tbNewListItems = new System.Windows.Forms.TextBox();
+            this.tbInterval = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lbStatus = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lbPlayListsAfterLoad = new System.Windows.Forms.Label();
+            this.btnLoadPlayLists = new System.Windows.Forms.Button();
+            this.btnAdsintoPlayLists = new System.Windows.Forms.Button();
+            this.tbFolderPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbNewListItems
+            // 
+            this.tbNewListItems.Location = new System.Drawing.Point(6, 42);
+            this.tbNewListItems.Multiline = true;
+            this.tbNewListItems.Name = "tbNewListItems";
+            this.tbNewListItems.Size = new System.Drawing.Size(294, 353);
+            this.tbNewListItems.TabIndex = 0;
+            this.tbNewListItems.Text = resources.GetString("tbNewListItems.Text");
+            // 
+            // tbInterval
+            // 
+            this.tbInterval.Location = new System.Drawing.Point(245, 16);
+            this.tbInterval.Name = "tbInterval";
+            this.tbInterval.Size = new System.Drawing.Size(55, 20);
+            this.tbInterval.TabIndex = 2;
+            this.tbInterval.Text = "5";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(96, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Interval between Ads Tracks";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.tbNewListItems);
+            this.groupBox1.Controls.Add(this.tbInterval);
+            this.groupBox1.Location = new System.Drawing.Point(482, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(306, 402);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Add Ads into Play Lists";
+            // 
+            // lbStatus
+            // 
+            this.lbStatus.AutoSize = true;
+            this.lbStatus.Location = new System.Drawing.Point(12, 425);
+            this.lbStatus.Name = "lbStatus";
+            this.lbStatus.Size = new System.Drawing.Size(143, 13);
+            this.lbStatus.TabIndex = 4;
+            this.lbStatus.Text = "Interval between Ads Tracks";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lbPlayListsAfterLoad);
+            this.groupBox2.Location = new System.Drawing.Point(16, 83);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(404, 324);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "List of Playa Lists";
+            // 
+            // lbPlayListsAfterLoad
+            // 
+            this.lbPlayListsAfterLoad.AutoSize = true;
+            this.lbPlayListsAfterLoad.Location = new System.Drawing.Point(6, 25);
+            this.lbPlayListsAfterLoad.Name = "lbPlayListsAfterLoad";
+            this.lbPlayListsAfterLoad.Size = new System.Drawing.Size(532, 13);
+            this.lbPlayListsAfterLoad.TabIndex = 5;
+            this.lbPlayListsAfterLoad.Text = "Intervalasdadsad asd ad ad asd ad asd asd asadasdadas asd asd ad asdas dasdasd as" +
+    "d  between Ads Tracks";
+            // 
+            // btnLoadPlayLists
+            // 
+            this.btnLoadPlayLists.Location = new System.Drawing.Point(16, 21);
+            this.btnLoadPlayLists.Name = "btnLoadPlayLists";
+            this.btnLoadPlayLists.Size = new System.Drawing.Size(174, 23);
+            this.btnLoadPlayLists.TabIndex = 7;
+            this.btnLoadPlayLists.Text = "Load All Play Lists";
+            this.btnLoadPlayLists.UseVisualStyleBackColor = true;
+            this.btnLoadPlayLists.Click += new System.EventHandler(this.btnLoadPlayLists_Click);
+            // 
+            // btnAdsintoPlayLists
+            // 
+            this.btnAdsintoPlayLists.Location = new System.Drawing.Point(246, 21);
+            this.btnAdsintoPlayLists.Name = "btnAdsintoPlayLists";
+            this.btnAdsintoPlayLists.Size = new System.Drawing.Size(174, 23);
+            this.btnAdsintoPlayLists.TabIndex = 8;
+            this.btnAdsintoPlayLists.Text = "Add Ads into Play Lists";
+            this.btnAdsintoPlayLists.UseVisualStyleBackColor = true;
+            this.btnAdsintoPlayLists.Click += new System.EventHandler(this.btnAdsintoPlayLists_Click);
+            // 
+            // tbFolderPath
+            // 
+            this.tbFolderPath.Location = new System.Drawing.Point(96, 54);
+            this.tbFolderPath.Name = "tbFolderPath";
+            this.tbFolderPath.Size = new System.Drawing.Size(324, 20);
+            this.tbFolderPath.TabIndex = 4;
+            this.tbFolderPath.Text = "C:\\Users\\Shahid\\Desktop\\temp\\Fiverr\\Final 1-1-2021 playlists\\";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "PlayList Path: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 447);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbFolderPath);
+            this.Controls.Add(this.btnAdsintoPlayLists);
+            this.Controls.Add(this.btnLoadPlayLists);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.lbStatus);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbNewListItems;
+        private System.Windows.Forms.TextBox tbInterval;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lbStatus;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnLoadPlayLists;
+        private System.Windows.Forms.Button btnAdsintoPlayLists;
+        private System.Windows.Forms.Label lbPlayListsAfterLoad;
+        private System.Windows.Forms.TextBox tbFolderPath;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdsIntoPlayList
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void ReadBothPlayListItemsandGeneratePlayListFile(string PlayListFileandPath)
+        {
+            List<string> currentPlayListFile = new List<string>();
+            List<string> refinePlayListFile = new List<string>();
+            List<string> currentPlayListForm = new List<string>();
+            List<string> refinePlayListForm = new List<string>();
+            List<string> processedPlayList = new List<string>();
+            //string PlayListFileandPath = @"C:\Users\Shahid\Desktop\temp\Fiverr\Final 1-1-2021 playlists\SEM Records 2020 Thursday Mania Keep Distance.m3u8";
+            string file = ReadPlayListFileAndReturnString(PlayListFileandPath);
+            currentPlayListFile = StringSplittoListbyNewLine(file);
+            refinePlayListFile = RefinePlayListbyCombineTwoLines(currentPlayListFile);
+            string form = tbNewListItems.Text;
+            currentPlayListForm = StringSplittoListbyNewLine(form);
+            refinePlayListForm = RefinePlayListbyCombineTwoLines(currentPlayListForm);
+            int currentItrationFormList = 0;
+            int intervalValue = int.Parse(tbInterval.Text);
+
+            processedPlayList.Add("#EXTM3U");
+            for (int i = 0; i < refinePlayListFile.Count; i++)
+            {
+                if (i == refinePlayListFile.Count)
+                {
+                    continue;
+                }
+                if (i == intervalValue)
+                {
+                    processedPlayList.Add(refinePlayListForm[currentItrationFormList]);
+                    intervalValue += int.Parse(tbInterval.Text);
+                    currentItrationFormList++;
+                    if (currentItrationFormList == refinePlayListForm.Count)
+                    {
+                        currentItrationFormList = 0;
+                    }
+                }
+                processedPlayList.Add(refinePlayListFile[i]);
+                //if (i == 0)
+                //{
+                //    for (; i < intervalValue; i++)
+                //    {
+                //        processedPlayList.Add(refinePlayListFile[i]);
+
+                //    }
+                //}
+                //if ((i % intervalValue) == 0)
+                //{
+                //    processedPlayList.Add(refinePlayListFile[i]);
+                //    processedPlayList.Add(refinePlayListForm[currentItrationFormList]);
+                //    if (currentItrationFormList == refinePlayListForm.Count - 1)
+                //    {
+                //        currentItrationFormList = 0;
+                //    }
+                //    else
+                //    {
+                //        currentItrationFormList++;
+                //    }
+                //}
+                //else
+                //{
+                //    processedPlayList.Add(refinePlayListFile[i]);
+                //}
+            }
+            var result = WritePlayListFileFromList(PlayListFileandPath, processedPlayList);
+        }
+
+        private List<string> StringSplittoListbyNewLine(string fileContent)
+        {
+            string stringSplitter = Environment.NewLine;
+            string[] stringSpliterArray = { stringSplitter };
+            return fileContent.Split(stringSpliterArray, StringSplitOptions.RemoveEmptyEntries).ToList();
+        }
+
+        private string ReadPlayListFileAndReturnString(string PlayListFileandPath)
+        {
+            string fileContent = String.Empty;
+            using (StreamReader reader = new StreamReader(PlayListFileandPath))
+            {
+                fileContent = reader.ReadToEnd();
+                reader.Dispose();
+                reader.Close();
+            }
+            return fileContent;
+        }
+
+        private bool WritePlayListFileFromList(string playListFileandPath, List<string> listToWrite)
+        {
+            try
+            {
+                // Full file name. Change your file name   
+                FileInfo fileName = new FileInfo(playListFileandPath);
+
+                // Get File Name  
+                string justFileName = fileName.Name;
+                // Get file extension   
+                string extn = fileName.Extension;
+
+                string fileNamewithoutExt = justFileName.Replace(extn, String.Empty);
+
+                //string playListStart = "#EXTM3U";
+                string dateTimeNow = "_" + DateTime.Now.ToString("yyyyMMdd_HHmmss");
+                //` tring playListExt = ".m3u8";
+                string finalFileName = fileNamewithoutExt + dateTimeNow + extn;
+
+                string finalCombinePathandFile = Path.Combine(fileName.DirectoryName, finalFileName);
+                //using (StreamWriter writer = new StreamWriter(finalCombinePathandFile, append: true))
+                //using (StreamWriter writer = new StreamWriter(finalCombinePathandFile))
+                using (StreamWriter writer = new StreamWriter(File.Open(finalCombinePathandFile, FileMode.Create), Encoding.Unicode))
+                {
+                    //writer.WriteLine(playListStart);
+                    foreach (var item in listToWrite)
+                    {
+                        writer.WriteLine(item);
+                    }
+                    writer.Dispose();
+                    writer.Close();
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString(), "Test Caption", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+
+        List<string> allPlayListsM3U8 = new List<string>();
+        List<string> RefinecurrentPlayListfromList = new List<string>();
+        List<string> RefinecurrentPlayListform = new List<string>();
+
+        private List<string> RefinePlayListbyCombineTwoLines(List<string> currentPlayList)
+        {
+            string listItemStart = "#EXTINF";
+            List<string> RefinePlayListfromList = new List<string>();
+            for (int i = 0; i < currentPlayList.Count; i++)
+            {
+                if (currentPlayList[i].StartsWith(listItemStart))
+                {
+                    RefinePlayListfromList.Add(currentPlayList[i] + Environment.NewLine + currentPlayList[i + 1]);
+                }
+            }
+            return RefinePlayListfromList;
+        }
+
+        static List<string> ReturnListofPlayListsFromFolder(string folderPath, out string textToDisplay)
+        {
+            //FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
+            //folderBrowser.Description = "Select PlayList folder";
+            //DialogResult dialogResults1 = folderBrowser.ShowDialog();
+            //OpenFileDialog openDialogBox = new OpenFileDialog();
+            //openDialogBox.Title = "Select PlayList Folder";
+            //openDialogBox.Filter = "Text Files (*.txt)|*.txt|All Files|*.*";
+
+            //DialogResult dialogResults = openDialogBox.ShowDialog();
+            List<string> allFileList = new List<string>();
+            List<string> filteredPlayLists = new List<string>();
+            string listOfAllPlayLists = String.Empty;
+            string path = folderPath;
+            //string path = @"C:\Users\Shahid\Desktop\temp\Fiverr\Final 1-1-2021 playlists\";
+            string playListExt = "m3u8";
+
+            //allFileList = Directory.GetFiles(@"C:\Users\Shahid\Desktop\temp\Fiverr\Final 1-1-2021 playlists\", "*", SearchOption.AllDirectories).ToList(); 
+            allFileList = Directory.GetFiles(path).ToList();
+
+            int loopCounter = 1;
+            foreach (var file in allFileList)
+            {
+                string onlyFileName = String.Empty;
+                onlyFileName = file.Replace(path, String.Empty);
+                if (onlyFileName.Contains(playListExt))
+                {
+                    filteredPlayLists.Add(file);
+                    listOfAllPlayLists += loopCounter.ToString() + ": " + onlyFileName + Environment.NewLine;
+                    loopCounter++;
+                }
+            }
+            textToDisplay = listOfAllPlayLists;
+            return filteredPlayLists;
+        }
+
+        private void btnLoadPlayLists_Click(object sender, EventArgs e)
+        {
+            string textToDisplayonForm;
+            allPlayListsM3U8 = ReturnListofPlayListsFromFolder(tbFolderPath.Text, out textToDisplayonForm);
+            lbPlayListsAfterLoad.Text = textToDisplayonForm;
+            //string path = folderBrowserDialog1.RootFolder.ToString();
+            //string path = openFileDialog1.SafeFileNames();
+            //string[] tempStrings = openFileDialog1.SafeFileNames;
+        }
+
+        private void btnAdsintoPlayLists_Click(object sender, EventArgs e)
+        {
+            ReadBothPlayListItemsandGeneratePlayListFile(@"C:\Users\Shahid\Desktop\temp\Fiverr\Final 1-1-2021 playlists\SEM Records 2020 Thursday Mania Keep Distance.m3u8");
+        }
+    }
+}
